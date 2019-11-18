7a3d21eb996b901a432d7f5991df8c89b7dadc69
Stanley Lee <sachih@live.com>
2019-11-18 23:59:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,9 +11,14 @@ def load_library(file_path)
   library
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
-  result = library["get_emoticon"]
+  result = library["get_emoticon"][emoticon]
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
+  end 
 end
 
 def get_english_meaning
